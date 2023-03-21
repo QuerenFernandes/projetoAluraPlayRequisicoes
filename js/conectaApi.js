00283284027aab3ba853a77007562eea0ccfa420
Queren Hapuque <queren.fernandes@yahoo.com>
2023-03-21 06:49:01 -0300

    arquivos iniciais e primeiras aulas

---
+++ js/conectaApi.js
@@ -0,0 +1,38 @@
+async function listaVideos(){
+    // requisicao get - solicitando valores da API
+    const conexao = await fetch("colocar caminho do computador");
+    const conexaoConvertida = await conexao.json();
+   
+    return conexaoConvertida;
+}
+
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("colocar caminho do computador", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem:imagem
+        })
+    });
+
+    const conexaoConvertida = await conversao.json();
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca){
+    const conexao = await fetch(`colocar caminho do computador?q=${termoDeBusca}`)
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}   
+
+export const conectaApi = {
+    listaVideos, 
+    criaVideo,
+    buscaVideo
+}
